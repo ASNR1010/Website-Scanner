069691c361cca0947d15afc4917d01ac92b058a0
Ankit Singh <52111453+ASNR1010@users.noreply.github.com>
2024-09-24 01:31:11 +0530

    Update general.py

--- general.py
+++ general.py
@@ -2,12 +2,12 @@ import os
 
 def create_dir(directory):
     if not os.path.exists(directory):
+        print("Creating directory...")
         os.makedirs(directory)
 
 
 def write_file(path, data):
     f = open(path, 'w')
+    print("Writing in to the file = ", f.name)
     f.write(data)
     f.close()
-
-
